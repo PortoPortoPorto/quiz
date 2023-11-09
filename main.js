0d7c93c898466756dfe2a11b85d1e83aa22738c0
PortoPortoPorto <tristanporterrobertson@gmail.com>
2023-11-09 17:54:58 +1100

    third commit, added retry

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+	const quizContainer = document.querySelector('#quiz-container'); 
+	const questionContainer = document.querySelector('#question-container'); 
+	const answerContainer = document.querySelector('#answer-container');
+	const optionBoxes = document.querySelectorAll('.option-box');
+	const scoreContainer = document.querySelector('#score-container'); 
+	const optionA = document.querySelector('#option-a');
+	const optionB = document.querySelector('#option-b'); 
+	const optionC = document.querySelector('#option-c'); 
+	const totalScore = document.querySelector('#total');  
+	const nextBox = document.querySelector('#next-box');
+	const next = document.querySelector('.next'); 
+	const initialScoreContainerHTML = scoreContainer.innerHTML; 
+	const initialQuizContainerHTML = quizContainer.innerHTML;
+	let score = 0; 
+	let clickCount = 0;
+	let currentQuestionIndex = 0;
+	let currentQuestion; 
+	let nextClicks = 0;   
 
-const questionContainer = document.querySelector('#question-container'); 
-const answerContainer = document.querySelector('#answer-container');
-const optionBoxes = document.querySelectorAll('.option-box');
-const scoreContainer = document.querySelector('#score-container'); 
-const optionA = document.querySelector('#option-a');
-const optionB = document.querySelector('#option-b'); 
-const optionC = document.querySelector('#option-c'); 
-const totalScore = document.querySelector('#total'); 
-const next = document.querySelector('#next'); 
-let score = 0; 
-let clickCount = 0;
-let currentQuestionIndex = 0;
-let currentQuestion;   
 
 //CONSTRUCTOR CLASS FOR QUESTIONS
 class Question {
@@ -24,15 +29,46 @@ class Question {
 
 //UI CLASS: HANDLES MAIN METHODS OF THE QUIZ 
 class UI {
+
+	static pageLoad() {
+
+	next.addEventListener('click', () => {
+		console.log('CLICKED');
+		nextClicks ++; 
+		console.log(nextClicks); 
+		UI.nextQuestion(); 
+	});
+
+	optionBoxes.forEach((box, index) => {
+		box.addEventListener('mouseover', () => {
+			if (clickCount === 0) {
+			box.style.backgroundColor = '#a3a7bf';
+			} 
+		});
+		box.addEventListener('mouseout', () => {
+			if (clickCount === 0) {
+				box.style.backgroundColor = '#656d8c';
+				}
+			});
+		box.addEventListener('click', (e) => {
+			console.log(e.target.textContent);
+			UI.displayAnswer(e.target.textContent); 
+			}); 
+		});	
+
+	}
+
+
 //DISPLAY A QUESTION BASED ON THE CURRENT QUESTION INDEX
 	static displayQuestion() { 
-		console.log(questions);
-		console.log(currentQuestionIndex);
+		const next = document.querySelector('.next'); 
+		next.style.display = 'none'; 
 		optionBoxes.forEach((box, index) => {
 			box.style.backgroundColor = '#656d8c';
-			clickCount = 0;  
-		})
-		if(currentQuestionIndex === 0) {
+			box.style.diplay = 'flex'; 
+
+		}); 
+		if(currentQuestionIndex === 0) {  
 			currentQuestion = new Question ('What is the capital of Switzerland?', ['Bern', 'Zurich', 'Lucerne'], 'Bern');
 		} else if (currentQuestionIndex === 1) {
 			currentQuestion = new Question ('Which of these countries borders Greece?', ['Serbia', 'Albania', 'Croatia'], 'Albania');
@@ -55,18 +91,26 @@ class UI {
 		} 	
 		
 		if (currentQuestion) {
-			console.log(currentQuestion);
+			const optionA = document.querySelector('#option-a');
+			const optionB = document.querySelector('#option-b'); 
+			const optionC = document.querySelector('#option-c'); 
+			const optionBoxes = document.querySelectorAll('.option-box');
+			const questionContainer = document.querySelector('#question-container'); 
 			questionContainer.innerHTML = currentQuestion.question;  
 			optionA.innerHTML = currentQuestion.options[0];
 			optionB.innerHTML = currentQuestion.options[1]; 
-			optionC.innerHTML = currentQuestion.options[2];
+			optionC.innerHTML = currentQuestion.options[2];			
 		}	
-		console.log(currentQuestion); 	
 	}
 
-
 	static displayAnswer(answer) {
-	// DISPLAY CORRECT ANSWER WITH BACKGROUND COLOR AND TEXT
+	     //SHOW NEXT BUTTON
+		const next = document.querySelector('.next'); 
+		next.style.display = 'flex'; 
+		// DISPLAY CORRECT ANSWER WITH BACKGROUND COLOR AND TEXT 
+		const nextBox = document.querySelector('#next-box'); 
+		nextBox.appendChild(next);
+		const optionBoxes = document.querySelectorAll('.option-box');
 		const currentQuest = currentQuestion.question;
 		const currentOpt = currentQuestion.options;
 		const currentCorrect = currentQuestion.correctAnswer; 
@@ -75,51 +119,47 @@ class UI {
 				box.style.backgroundColor = '#658c69'; 
 			} else if (box.textContent !== currentCorrect) {
 				box.style.backgroundColor = '#8c4c49';
-			} else {
-				questionContainer.innerHTML = 'click for the next question';
-			}
+			} 
 		});
 		// IF CLICKED ANSWER IS CORRECT, ADD TO TOTAL SCORE. MAINTAIN CLICK COUNT  
+		const questionContainer = document.querySelector('#question-container'); 
 		if(answer === currentCorrect && clickCount === 0) {
 			questionContainer.innerHTML = 'Correct!';
 			const total = document.querySelector('#total');
 			score ++; 
 			clickCount++;
 			total.innerHTML = `Total Score: ${score}`; 
+			console.log(currentQuestionIndex);			
 		} else if (clickCount === 0) {
 			questionContainer.innerHTML = 'Wrong!';
 			total.innerHTML = `Total Score: ${score}`;
 			clickCount++;
-		} else {
-			questionContainer.innerHTML = 'Click for the next question';
-		}
-	}
-
+			console.log(currentQuestionIndex);			
+		} 
+	}			
+	
 
 	static nextQuestion() {
+
 		//INCREMENT TO THE NEXT QUESTION 
 		//DISPLAY THE CURRENT QUESTION IF CLICKCOUNT IS > 0
+		const next = document.querySelector('.next');  
+		next.style.display = 'none'; 
+		console.log('next question initialized'); 	
 		if (clickCount > 0) { 
+			clickCount = 0; 
 			currentQuestionIndex ++;
 			currentQuestion = questions[currentQuestionIndex]; 
 			UI.displayQuestion();
-		} else {
-			questionContainer.innerHTML = 'please answer the current question';
-			questionContainer.id = '';
-			questionContainer.classList.add('alert'); 
-			setTimeout(function() {
-				questionContainer.innerHTML = currentQuestion.question;
-				questionContainer.classList.remove('alert'); 
-				questionContainer.id = 'question-container';
-			}, 2000);
-
-			//setTimeout(() => questionContainer.innerHTML = currentQuestion.question, 2500); 
-		} 
+			} 
+		 
 		//PROVIDE FINAL SCORE AT END OF QUESTIONS ARRAY
 		if(currentQuestionIndex > 9) {
+			currentQuestionIndex = 0;  
 			UI.finalScore(); 
 		}
 	}
+
 
 	static finalScore() {
 		//INCREASE THE SIZE OF THE FINAL SCORE & REMOVE OTHER DISPLAY CONTAINERS
@@ -131,22 +171,38 @@ class UI {
 			scoreContainer.innerHTML = `
 			<h1>Your total score is ${score}<h1>
 			<h1>You are NOT a smart genius.<h1>
-			<img  id = "sad-ethan" src = "images/sadethan.jpg" alt = "sad-Ethan-image">`
+			<div><img  id = "sad-ethan" src = "images/sadethan.jpg" alt = "sad-Ethan-image"></div>
+			<div id = "retry-box"><button id = "retry">Retry</button></div>`
+			const retry = document.querySelector('#retry');
+			retry.addEventListener('click', () => {
+				UI.resetQuiz();
+			}); 
+			
 		} else if (score > 5 && score < 9) {
 			scoreContainer.style.color = '#bdbfc9'
 			scoreContainer.innerHTML = `
 			<h1>Your total score is ${score}<h1>
 			<h1>You are a MEDIUM LEVEL smart genius.<h1>
-			<img id = "pleased-ethan" src = "images/pleasedethan.jpg" alt = "pleased-Ethan-image">`
+			<div><img id = "pleased-ethan" src = "images/pleasedethan.jpg" alt = "pleased-Ethan-image"></div>
+			<div id = "retry-box"><button id = "retry">Retry</button></div>`
+			document.querySelector('#retry').addEventListener('click', () => {
+			UI.resetQuiz(); 
+			});
 		} else {
 			scoreContainer.style.color = '#e3c868'
 			scoreContainer.innerHTML = `
 			<h1>Your total score is ${score}<h1>
 			<h1>CONGRATULATIONS! You are a SMART GENIUS!<h1>
-			<img id = "genius-ethan" src = "images/geniusethan.gif" alt = "genius-Ethan-gif">`
+			<div><img id = "genius-ethan" src = "images/geniusethan.gif" alt = "genius-Ethan-gif"></div>
+			<div id = "retry-box"><button id = "retry">Retry</button></div>`
+			document.querySelector('#retry').addEventListener('click', () => {
+			UI.resetQuiz(); 
+			});
 		}
-		
-		// if the score is 1-5 give negative message / 6-8 positive / 9-10 extremely positive message 
+	}
+
+	static resetQuiz() {
+		location.reload(); 
 	}
 }
 
@@ -163,29 +219,11 @@ const question10 = new Question ('Which is the most expensive city to live in gl
 const questions = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10',];
 
 // display first question on load
+UI.pageLoad(); 
 UI.displayQuestion();
-console.log(currentQuestion); 
+
  
 
 // event listener to display correct answer on answer selection
-optionBoxes.forEach((box) => {
-	box.addEventListener('mouseover', () => {
-		if (clickCount === 0) {
-		box.style.backgroundColor = '#a3a7bf';
-		} 
-	});
-	box.addEventListener('mouseout', () => {
-		if (clickCount === 0) {
-		box.style.backgroundColor = '#656d8c';
-		}
-	});
-	box.addEventListener('click', (e) => {
-		console.log(e.target.textContent);
-		UI.displayAnswer(e.target.textContent); 
-	}); 
-});
-// event listener on button click for next question 
-	next.addEventListener('click', (e) => {
-		UI.nextQuestion(); 
-	});
 
+	
